4dbdd594dd65268f0b40a7eeecd9c3730e52d9f9
AlfreoEv2 <alfredosalvadorestradavega@hotmail.com>
2024-11-27 04:12:49 +0000

    Retry Startup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-// Copyright 2024 enzoames. All Rights Reserved.
-
-require('dotenv').config();
-const axios = require('axios');
-const { CronJob } = require('cron');
-const { Client, GatewayIntentBits, Events, AuditLogEvent } = require('discord.js');
-const { PromisePool } = require('@supercharge/promise-pool');
-const get = require('lodash.get');
-const log = require('./utils/log');
-const config = require('./utils/config');
-const { ADD, REMOVE } = require('./utils/constants');
-const express = require('express');
+require("dotenv").config();
+const axios = require("axios");
+const { CronJob } = require("cron");
+const {
+  Client,
+  GatewayIntentBits,
+  Events,
+  AuditLogEvent,
+} = require("discord.js");
+const { PromisePool } = require("@supercharge/promise-pool");
+const get = require("lodash.get");
+const log = require("./utils/log");
+const config = require("./utils/config");
+const { ADD, REMOVE } = require("./utils/constants");
+const express = require("express");
 const app = express();
 app.use(express.json());
+
+const MAX_RETRIES = 3;
+const RETRY_DELAY = 5000; // 5 seconds
 
 // creates new client
 const client = new Client({
@@ -25,47 +31,51 @@ const client = new Client({
 });
 
 const getEasterDateTime = () =>
-  new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
+  new Date().toLocaleString("en-US", { timeZone: "America/New_York" });
 
 const getExpiredRoles = async (roleId) => {
   try {
     const resp = await axios({
-      method: 'get',
+      method: "get",
       url: `${config.baseUrl}/rt/roles/${roleId}/expired`,
     });
     return resp.data;
   } catch (e) {
-    log.error('[ERROR] status:', e?.response?.status);
-    log.error('[ERROR] response data:', e?.response?.data || e);
+    log.error("[ERROR] status:", e?.response?.status);
+    log.error("[ERROR] response data:", e?.response?.data || e);
   }
 };
 
 const postAuditLog = async (data) => {
   try {
-    const resp = await axios({ method: 'post', url: `${config.baseUrl}/rt/auditlogs`, data });
-    log.message('[LOG] response status:', resp.status);
+    const resp = await axios({
+      method: "post",
+      url: `${config.baseUrl}/rt/auditlogs`,
+      data,
+    });
+    log.message("[LOG] response status:", resp.status);
   } catch (e) {
-    log.error('[ERROR] status:', e?.response?.status);
-    log.error('[ERROR] response data:', e?.response?.data || e);
+    log.error("[ERROR] status:", e?.response?.status);
+    log.error("[ERROR] response data:", e?.response?.data || e);
   }
 };
 
 client.on(Events.ClientReady, async () => {
-  log.message('[LOG] Logged in as:', client.user.tag);
+  log.message("[LOG] Logged in as:", client.user.tag);
 
   // every day at 07:00 => 0 7 * * 0-6
   // every minute => */1 * * * *
   const job = new CronJob(
-    '0 7 * * 0-6',
+    "0 7 * * 0-6",
     async () => {
-      log.message('\n[LOG] running job: checking for expired roles');
-      log.message('[LOG]', getEasterDateTime());
+      log.message("\n[LOG] running job: checking for expired roles");
+      log.message("[LOG]", getEasterDateTime());
 
       // get expired roles for each roleId
       const { results } = await PromisePool.for(config.roles)
         .withConcurrency(1)
         .process(async (r) => {
-          log.message('[LOG] fetching expired roles for:', r);
+          log.message("[LOG] fetching expired roles for:", r);
           const resp = await getExpiredRoles(r);
           return resp;
         });
@@ -78,14 +88,14 @@ client.on(Events.ClientReady, async () => {
         .process(async (eR) => {
           const guild = await client.guilds.fetch(config.serverId);
           const guildMember = await guild.members.fetch(eR.userId);
-          log.message('\n[LOG] removing username:', guildMember.user.username);
+          log.message("\n[LOG] removing username:", guildMember.user.username);
           guildMember.roles.remove(eR.roleId, `role expired ${eR.endDate}`);
           // force await here for the time between network requests from discord api and our api
           await new Promise((resolve) => setTimeout(resolve, 3000));
         });
     },
     ...[, ,],
-    'America/New_York'
+    "America/New_York"
   );
 
   job.start();
@@ -96,27 +106,28 @@ client.on(Events.GuildAuditLogEntryCreate, async (auditLog) => {
   const validateAction = action !== AuditLogEvent.MemberRoleUpdate;
 
   if (validateAction) {
-    log.message('\n[LOG] event not processed', action);
+    log.message("\n[LOG] event not processed", action);
     return;
   }
 
-  const currentChange = get(changes, '[0]', {});
+  const currentChange = get(changes, "[0]", {});
   const validateChange = changes.length > 1 && currentChange?.new.length > 1;
 
   if (validateChange) {
-    log.message('\n[LOG] invalid change');
+    log.message("\n[LOG] invalid change");
     return;
   }
 
-  const newRoleId = get(currentChange, 'new[0].id', '');
+  const newRoleId = get(currentChange, "new[0].id", "");
   const validateRoleId = !config.roles.includes(newRoleId);
 
   if (validateRoleId) {
-    log.message('\n[LOG] role not processed');
+    log.message("\n[LOG] role not processed");
     return;
   }
 
-  const isValidOperation = currentChange.key === ADD || currentChange.key === REMOVE;
+  const isValidOperation =
+    currentChange.key === ADD || currentChange.key === REMOVE;
 
   if (isValidOperation) {
     const data = {
@@ -125,9 +136,9 @@ client.on(Events.GuildAuditLogEntryCreate, async (auditLog) => {
       change: { ...currentChange },
     };
 
-    log.message('\n[LOG] operation:', currentChange.key);
-    log.message('[LOG] execution time:', getEasterDateTime());
-    log.message('[LOG] post data:', JSON.stringify(data));
+    log.message("\n[LOG] operation:", currentChange.key);
+    log.message("[LOG] execution time:", getEasterDateTime());
+    log.message("[LOG] post data:", JSON.stringify(data));
     await postAuditLog(data);
   }
 });
@@ -151,7 +162,7 @@ const createInvite = async (channel, role) => {
   }
 };
 
-app.post('/create-invite', async (req, res) => {
+app.post("/create-invite", async (req, res) => {
   try {
     console.log(req.body);
     const { channelId, roleName } = req.body;
@@ -185,14 +196,46 @@ client.on("guildMemberAdd", async (member) => {
         await member.roles.add(role);
         await usedInvite.delete();
         delete inviteRoles[usedInvite.code];
-      } 
+      }
     }
   } catch (error) {
     console.error("Error in guildMemberAdd event handler:", error);
   }
 });
 
+async function startBot() {
+  let retries = 0;
+  while (retries < MAX_RETRIES) {
+    try {
+      if (!config.botToken) {
+        throw new Error("Bot token is missing in the configuration");
+      }
+      await client.login(config.botToken);
+      log.message("[LOG] Bot successfully logged in");
 
-client.login(config.botToken);
+      // Start the server after successful login
+      const port = config.port || 8142;
+      app.listen(port, () => {
+        log.message(`[LOG] Server is running on port ${port}`);
+      });
 
-app.listen(config.port, () => {console.log(`Server is running on port ${config.port}`);});
+      return; // Exit the function if login is successful
+    } catch (error) {
+      retries++;
+      log.error(
+        `[ERROR] Failed to log in (Attempt ${retries}/${MAX_RETRIES}):`,
+        error.message
+      );
+      if (retries < MAX_RETRIES) {
+        log.message(`[LOG] Retrying in ${RETRY_DELAY / 1000} seconds...`);
+        await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY));
+      } else {
+        log.error("[ERROR] Max retries reached. Unable to start the bot.");
+        process.exit(1); // Exit the process with an error code
+      }
+    }
+  }
+}
+
+// Call the function to start the bot
+startBot();
